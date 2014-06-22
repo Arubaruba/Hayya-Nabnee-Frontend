791f2ed0340daaf87d714ab7543bfd5579a060ee
Arubaruba <shadowstep7@gmail.com>
2014-06-22 11:11:56 +0300

    Angular uses it's own Routing

--- web/app.dart
+++ web/app.dart
@@ -1,6 +1,20 @@
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
+import 'package:angular/routing/module.dart'
+
+void initAppRouter(Router router, ViewFactory view) {
+  router.root
+    ..addRoute(
+      defaultRoute: true,
+      name: 'projects',
+      path: '/projects',
+      enter: './views/projects.html')
+}
+
+class AppModule extends Module {
+  value(RouteInitializerFn, initAppRouter);
+}
 
 main() {
-  applicationFactory().run();
+  applicationFactory(new AppModule()).run();
 }
