f8d45b6c28feaf1ab9e7c8889aca2a4fe0a576c8
Arubaruba <shadowstep7@gmail.com>
2014-06-22 17:13:29 +0300

    Works with backend server

--- web/controllers/project_list.dart
+++ web/controllers/project_list.dart
@@ -3,8 +3,7 @@ part of app;
 @Controller(selector: '[project-list]', publishAs: 'ctrl')
 class ProjectList {
   ProjectList() {
-
-    HttpRequest.getString('fake_accounts.json')
+    HttpRequest.getString('/backend')
       .then((String response) {
         JsonDecoder decoder = new JsonDecoder();
         print(decoder.convert(response).last['name']);
--- web/modules/project_list.dart
+++
@@ -1,6 +0,0 @@
-part of app;
-
-@Controller(selector: '[project-list]', publishAs: 'ctrl')
-class ProjectList {
-  String test = '1234';
-}
