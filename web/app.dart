73897007b1cb6c0bb1cbffc904415a26a09ae2d4
Arubaruba <shadowstep7@gmail.com>
2014-06-22 12:15:20 +0300

    Router works, but cannot load views correctly

--- web/app.dart
+++ web/app.dart
@@ -1,20 +1,25 @@
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
-import 'package:angular/routing/module.dart'
-
-void initAppRouter(Router router, ViewFactory view) {
-  router.root
-    ..addRoute(
-      defaultRoute: true,
-      name: 'projects',
-      path: '/projects',
-      enter: './views/projects.html')
-}
+import 'package:angular/routing/module.dart';
 
 class AppModule extends Module {
-  value(RouteInitializerFn, initAppRouter);
+  AppModule() {
+    value(RouteInitializerFn, new AppRouter());
+  }
+}
+
+class AppRouter implements RouteInitializer {
+
+  call(Router router, ViewFactory view) {
+    router.root
+      ..addRoute(
+        defaultRoute: true,
+        name: 'projects',
+        path: '/projects',
+        enter: view('views/projects.html'));
+  }
 }
 
 main() {
-  applicationFactory(new AppModule()).run();
+  applicationFactory().addModule(new AppModule()).run();
 }
