d009fd162643fd3994c4943edda1e6d4e5aed4f1
Arubaruba <shadowstep7@gmail.com>
2014-06-22 13:27:25 +0300

    Controller, fake_accounts (test) JSON file added

--- web/app.dart
+++ web/app.dart
@@ -1,9 +1,15 @@
+library app;
+
+import 'dart:html';
 import 'package:angular/angular.dart';
 import 'package:angular/application_factory.dart';
 import 'package:angular/routing/module.dart';
 
+part 'modules/project_list.dart';
+
 class AppModule extends Module {
   AppModule() {
+    type(ProjectList);
     value(RouteInitializerFn, new AppRouter());
   }
 }
@@ -12,11 +18,17 @@ class AppRouter implements RouteInitializer {
 
   call(Router router, ViewFactory view) {
     router.root
+
       ..addRoute(
         defaultRoute: true,
         name: 'projects',
         path: '/projects',
-        enter: view('views/projects.html'));
+        enter: view('views/project_list.html'))
+
+      ..addRoute(
+        name: '',
+        path: '/project/:projectId',
+        enter: view('views/project.html'));
   }
 }
 
---
+++ web/modules/project_list.dart
@@ -0,0 +1,6 @@
+part of app;
+
+@Controller(selector: '[project-list]', publishAs: 'ctrl')
+class ProjectList {
+  String test = '1234';
+}
